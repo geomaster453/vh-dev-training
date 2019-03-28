6f84a6befefaaca3011776f27fddde9a168c2d84
geomaster453 <austin.wei@lakesideschool.org>
2019-03-27 20:05:16 -0500

    HW 1 V2

--- week1/hw/hw.ts
+++ week1/hw/hw.ts
@@ -22,35 +22,39 @@ const objectively = () => {
 	console.log(console);
 	// Part 2
 	const whatAmI = { 0: 'A', 1: 'B', 2: 'C', 3: 'D' };
+	console.log(whatAmI);
 	// Part 3
 	(async () => {
-		let response = <[{
-			name: string, score: number,
-			spots: number, spotted: number, invalidated: number, unique: number,
-			id: string
-		}]>(await getDataFromAPI('https://spot.benc.me/?time=1549939921'))// sample call - delete before you submit
-
+		const response = (await getDataFromAPI('https://spot.benc.me/?time=1549939921')) as [
+			{
+				name: string;
+				score: number;
+				spots: number;
+				spotted: number;
+				invalidated: number;
+				unique: number;
+				id: string;
+			}
+		];
 		const rankByUnique = (n: number) => {
-			response.sort(
-				function (a, b) {
-					const comp = b.unique - a.unique
-					if (comp !== 0) {
-						return comp
-					}
-					return b.score - a.score
-				})
-			return response[n - 1]
-		}
+			response.sort(function(a, b) {
+				const comp = b.unique - a.unique;
+				if (comp !== 0) {
+					return comp;
+				}
+				return b.score - a.score;
+			});
+			return response[n - 1];
+		};
 		// Testing
-
-		//console.log("Leaderboard according to rankByUnique: ")
-		//console.log(rankByUnique(1));
-		//console.log(rankByUnique(2));
-		//console.log(rankByUnique(3));
-		//console.log(rankByUnique(4));
-		//console.log(rankByUnique(5));
+		console.log('Leaderboard according to rankByUnique: ');
+		console.log(rankByUnique(1));
+		console.log(rankByUnique(2));
+		console.log(rankByUnique(3));
+		console.log(rankByUnique(4));
+		console.log(rankByUnique(5));
 	})();
-}
+};
 objectively();
 
 const awry = () => {
@@ -58,45 +62,56 @@ const awry = () => {
 	for (let i = 0; i < 1000; i++) docTester[i] = Math.floor(Math.random() * 100);
 
 	// Part 1
-	let docTesterIndex: number[] = [];
+	const docTesterIndex: number[] = [];
 
-	docTester.forEach((i, index) => docTesterIndex[index] = i + index);
+	docTester.forEach(function(i, index) {
+		docTesterIndex[index] = i + index;
+	});
 
 	// Part 2
-	docTester.map((i, index) => docTesterIndex[index] = i + index);
+	// docTesterIndex.map(function(i, index) {
+	//	return docTester[index] + index;
+	// });
 
 	// Part 3
-	// console.log("Check sum: ");
-	console.log(docTester.map((i, index) => docTesterIndex[index] = i + index).reduce((acc, i) => (acc += i), 0));
+	console.log('Check sum: ');
+	console.log(
+		docTesterIndex.map((i, index) => docTester[index] + index).reduce((acc, i) => (acc += i), 0)
+	);
 
 	// Part 4
 	(async () => {
-		let response = <[{
-			name: string, score: number,
-			spots: number, spotted: number, invalidated: number, unique: number,
-			id: string
-		}]>(await getDataFromAPI('https://spot.benc.me/?time=1549939921'))// sample call - delete before you submit
+		const response = (await getDataFromAPI('https://spot.benc.me/?time=1549939921')) as [
+			{
+				name: string;
+				score: number;
+				spots: number;
+				spotted: number;
+				invalidated: number;
+				unique: number;
+				id: string;
+			}
+		];
 
 		const rankByUniqueAndFiltered = (n: number) => {
-			let newresponse = response.filter(entry => entry.spotted >= 3 || entry.spotted == 0).sort(
-				function (a, b) {
-					const n = b.unique - a.unique
-					if (n !== 0) {
-						return n
+			const newresponse = response
+				.filter(entry => entry.spotted >= 3 || entry.spotted === 0)
+				.sort(function(a, b) {
+					const comp = b.unique - a.unique;
+					if (comp !== 0) {
+						return comp;
 					}
-					return b.score - a.score
-				})
-			return newresponse[n - 1]
-		}
+					return b.score - a.score;
+				});
+			return newresponse[n - 1];
+		};
 		// Testing
-
-		//console.log("Leaderboard according to rankByUniqueAndFiltered: ")
-		//console.log(rankByUniqueAndFiltered(1));
-		//console.log(rankByUniqueAndFiltered(2));
-		//console.log(rankByUniqueAndFiltered(3));
-		//console.log(rankByUniqueAndFiltered(4));
-		//console.log(rankByUniqueAndFiltered(5));
+		console.log('Leaderboard according to rankByUniqueAndFiltered: ');
+		console.log(rankByUniqueAndFiltered(1));
+		console.log(rankByUniqueAndFiltered(2));
+		console.log(rankByUniqueAndFiltered(3));
+		console.log(rankByUniqueAndFiltered(4));
+		console.log(rankByUniqueAndFiltered(5));
 	})();
-}
+};
 awry();
-
